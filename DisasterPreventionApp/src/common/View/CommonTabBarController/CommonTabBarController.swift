d8a28c4e7a4da3d7ba6a102c1bddbc3c22624611
Masaki Sugita <choco_9031@yahoo.co.jp>
2021-02-27 22:58:50 +0900

    add src

---
+++ DisasterPreventionApp/src/common/View/CommonTabBarController/CommonTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  CommonTabBarController.swift
+//  DisasterPreventionApp
+//
+//  Created by Masaki on R 3/02/27.
+//
+
+import UIKit
+
+class CommonTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DisasterPreventionApp/src/common/View/TimeLine/TimeLineViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimeLineViewController.swift
+//  DisasterPreventionApp
+//
+//  Created by Masaki on R 3/02/27.
+//
+
+import UIKit
+
+class TimeLineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
